65f9b30aec695391e2a561f121232074727e7da1
Gadzata <andrijan-kata@hotmail.com>
2022-09-20 20:41:35 +0200

    Final

--- app/src/main/java/com/quizapp/Constants.kt
+++ app/src/main/java/com/quizapp/Constants.kt
@@ -5,12 +5,9 @@ import com.quizapp.Question
 
 object Constants {
 
-    // TODO (STEP 1: Create a constant variables which we required in the result screen.)
-    // START
     const val USER_NAME: String = "user_name"
     const val TOTAL_QUESTIONS: String = "total_questions"
     const val CORRECT_ANSWERS: String = "correct_answers"
-    // END
 
     fun getQuestions(): ArrayList<Question> {
         val questionsList = ArrayList<Question>()
